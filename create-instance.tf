a5cb6522309ebdeae43f06e143a1aaf61bc73131
sravan-github <39382490+sravan-github@users.noreply.github.com>
2022-09-01 18:55:30 +0530

    Update create-instance.tf

--- create-instance.tf
+++ create-instance.tf
@@ -56,7 +56,10 @@ metadata = {
 
 }
 
-
+output "avengers" {
+  value = null_resource.defaults
+}
+/*
 output "ip" {
   value = "${google_compute_instance.default.network_interface.0.access_config.0.nat_ip}"
-}
+} */
