1efccd8392a133c2a7165746e30a688c755a3af8
sravan-github <39382490+sravan-github@users.noreply.github.com>
2022-09-01 18:32:57 +0530

    Update terraform.sh

--- terraform.sh
+++ terraform.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-git clone https://github.com/sravan-github/terraformgcp.git
-cd terraformgcp
+git clone https://github.com/sravan-github/terraform_for_each.git
+cd terraform_for_each
 ls -l
 pwd
 export GOOGLE_APPLICATION_CREDENTIALS="./gcp-key.json"
